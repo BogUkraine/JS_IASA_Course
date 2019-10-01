1280c78a75e0b9fc2437e37aec619ce896e55f69
Bohdan Lysov <BogUkraine@gmail.com>
2019-10-01 22:00:36 +0300

    fixed form and small asider

--- Lab 1/2/scripts/aside.js
+++ Lab 1/2/scripts/aside.js
@@ -1,7 +1,7 @@
 const button = document.getElementById('aside-button');
-const asideBig = document.getElementsByClassName('aside-big');
-const asideSmall = document.getElementsByClassName('aside-small');
-const main = document.getElementsByTagName('main');
+const asideBig = document.getElementsByClassName('wrapper-aside-big');
+const asideSmall = document.getElementsByClassName('wrapper-aside-small');
+const main = document.getElementsByClassName('wrapper');
 const wrapper = document.getElementsByClassName('wrapper-recomendation__videos');
 
 let isAside = true;
@@ -11,16 +11,13 @@ button.onclick = () => {
     if(isAside) {
         asideBig[0].style.display = 'none';
         asideSmall[0].style.display = 'flex';
-        main[0].style.paddingLeft = '120px';
-        main[0].style.paddingRight = '180px';
-        //wrapper[0].style.justifyContent = 'center';
+        main[0].style.paddingLeft = '80px';
         isAside = false;
     }
     else {
         asideBig[0].style.display = 'flex';
         asideSmall[0].style.display = 'none';
-        main[0].style.paddingLeft = '240px';
-        main[0].style.paddingRight = '100px';
+        main[0].style.paddingLeft = '0';
         isAside = true;
     }
 
