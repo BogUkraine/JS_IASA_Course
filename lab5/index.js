121fd0a8b683156995219cf593f482de787ad879
Bohdan Lysov <BogUkraine@gmail.com>
2019-12-26 22:47:04 +0200

    deleted excess files and folders

--- lab5/index.js
+++ lab5/index.js
@@ -1,30 +1,92 @@
-const sizesWidth = document.getElementById('width');
-const sizesHeight = document.getElementById('height');
-const border = document.getElementById('border');
-const borderColor = document.getElementById('border-color');
-const altText = document.getElementById('alt-text');
-const button = document.getElementById('submit');
-const image = document.getElementById('image');
+const button = document.getElementById('addCompany');
+const table = document.getElementById('table__body');
 
-const arr = [sizesWidth, sizesHeight, border, borderColor, altText];
+let arr = [{
+    company: "Comp1",
+    income: 10
+},
+{
+    company: "Comp2",
+    income: 20
+},];
 
-button.addEventListener('click', ()=> {
-    let isChecker = true;
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
     
-    for (let item of arr) {
-        if(!item.checkValidity() || item.value === '') {
-            item.style.border = '2px solid red';
-            isChecker = false;
-        }
-        else {
-            item.style.border = '2px solid green';
-        }
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
     }
 
-    if(isChecker) {
-        image.style.width = sizesWidth.value + 'px';
-        image.style.height = sizesHeight.value + 'px';
-        image.style.border = border.value + 'px' + ' solid ' + borderColor.value;
-        image.setAttribute('alt', altText);
-    }
+    return color;
+}
+
+const addRow = (company, income) => {
+    const tr = document.createElement('tr');
+    const tdFirst = document.createElement('td');
+    const tdSecond = document.createElement('td');
+    const tdThird= document.createElement('td');
+
+    tdFirst.className = "table__delete";
+    tdFirst.textContent = "Delete";
+    tdSecond.textContent = `${company}`;
+    tdThird.textContent = `${income}`;
+
+    tdSecond.setAttribute('contenteditable', 'true');
+    tdThird.setAttribute('contenteditable', 'true');
+
+    table.append(tr);
+    tr.append(tdFirst);
+    tr.append(tdSecond);
+    tr.append(tdThird);
+
+    const diagramBlock = document.getElementById('diagram_block');
+    const diagram = document.createElement('div');
+    const diagramWrap = document.createElement('div');
+    const description = document.createElement('span');
+    const alt = document.createElement('span');
+
+    diagramWrap.className = "diagram";
+    diagram.className = "diagram__column";
+    description.className = "diagram__description";
+    alt.className = "diagram__alt";
+
+    diagramBlock.append(diagramWrap);
+    diagramWrap.append(diagram);
+    diagramWrap.append(description);
+    diagramWrap.append(alt);
+
+    description.textContent = `${company}`;
+    alt.textContent = `${income}$`;
+
+    diagram.style.height = `${income}px`;
+    diagram.style.backgroundColor = getRandomColor();
+
+    diagram.addEventListener('mouseover', () => {
+        alt.style.visibility = 'visible';
+    })
+
+    diagram.addEventListener('mouseout', () => {
+        alt.style.visibility = 'hidden';
+    })
+
+    tdSecond.addEventListener('input', () => {
+        description.textContent = tdSecond.textContent;
+    });
+
+    tdThird.addEventListener('input', () => {
+        diagram.style.height = tdThird.textContent + 'px';
+        alt.textContent = tdThird.textContent + '$';
+    });
+
+    tdFirst.addEventListener('click', () => {
+        tr.remove();
+        diagramWrap.remove();
+    })
+};
+
+
+button.addEventListener('click', () => {
+    addRow("Comp", 100);
 });
+
